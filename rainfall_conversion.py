2eebc044b4b3d5355fb812b1dbe6c3dc10f02f1a
Nina Meddings <nm5g14@soton.ac.uk>
2018-04-13 15:13:54 +0100

    Add rainfall module

---
+++ rainfall_conversion.py
@@ -0,0 +1,10 @@
+"""A library to perform rainfall unit conversion"""
+
+def inches_to_mm(inches)
+	"""Convert inches to millimetres.
+	Arguments:
+	inches --the rainfall in inches
+	"""
+	
+	mm = inches * 25.4
+	return mm
